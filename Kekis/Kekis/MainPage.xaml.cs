d90d46d79c0e795cf39af00ec99f3421f35d9627
Gerich1337 <rsuhanov2407@gmail.com>
2022-01-21 23:35:20 +0300

    \\

---
+++ Kekis/Kekis/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kekis
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            Image1.Source = ImageSource.FromResource("Kekis.gerich.jpg");
+            
+        }
+
+        private void btnEntry_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnReg_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
